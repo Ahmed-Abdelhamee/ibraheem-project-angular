091e0f3b98599049b7f29aa5811f89295f6ceccc
Ibrahim Ahmed <ibrahimahmed90000@gmail.com>
2023-11-08 18:39:32 +0200

    Prepare the products container

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { DocumentsComponent } from './Components/documents/documents.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent, title: 'Home page' },
+  {
+    path: 'home/products',
+    component: ProductsComponent,
+    title: 'All products',
+  },
+  {
+    path: 'home/documents',
+    component: DocumentsComponent,
+    title: 'All products',
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent, title: 'Not Found' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
